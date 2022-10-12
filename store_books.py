c525297d95a5499a5000166361dcfce1ea7e0d06
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2022-10-12 13:15:30 +0530

    Books are stored by user input

---
+++ store_books.py
@@ -0,0 +1,23 @@
+b1=input("Enter book number 1:\n")
+b2=input("Enter book number 2:\n")
+b3=input("Enter book number 3:\n")
+b4=input("Enter book number 4:\n")
+b5=input("Enter book number 5:\n")
+
+store_books=[b1,b2,b3,b4,b5,]
+print(store_books)
+
+
+''' Enter book number 1:
+computer
+Enter book number 2:
+java
+Enter book number 3:
+c
+Enter book number 4:
+evs
+Enter book number 5:
+data structure
+['computer', 'java', 'c', 'evs', 'data structure']
+
+'''
