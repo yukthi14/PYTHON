ec44b8c13839143d474c175935cf9fca8d68f7f9
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2022-10-12 13:14:45 +0530

    Few list methods

---
+++ list_methods.py
@@ -0,0 +1,8 @@
+l=[10,37,15,89,4,67,30]
+l.sort()     #sorts the element
+l.append(14) #adds the element in the last
+l.reverse()  #revese the given element
+l.insert(2,100) #inserts the 100 in index 2
+l.pop(0)   #removes the element of index 0
+l.remove(4) #removes 4 from the list
+print(l)
