7ac7fac75d8224162788826fcd43caf546a2b1e1
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2022-10-12 12:05:58 +0530

    Variables in python

---
+++ variable.py
@@ -0,0 +1,15 @@
+x='''yukthi'''   # multi-line useage
+r="achar"        #single line
+y=14             #Integer
+z=24.7978        #Long
+t=True           #Boolean
+
+print(x)
+print(r)
+print(y)
+print(z)
+print(t)
+
+print(type(x))  # automatically genrates the type
+
+# we need not specify whether its int,char,float,long,boolean it automatically senses it.
