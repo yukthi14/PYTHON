8ede447c7de6922d0f747b60f79e62f76fc0d587
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2022-10-13 16:59:15 +0530

    Whether the element is present or not

---
+++ list_present.py
@@ -0,0 +1,7 @@
+name1=['yukthi','vishwajeet','setu','sonu','bharti']
+name=input("enter the name to check\n")
+
+if name in name1:
+    print("your name is present")
+else:
+    print("your name is not present")
