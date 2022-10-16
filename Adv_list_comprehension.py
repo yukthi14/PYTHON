ad1297495946e36f8c1c7c0fe092e9e01e901071
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2022-10-16 14:08:33 +0530

    list comperhension

---
+++ Adv_list_comprehension.py
@@ -0,0 +1,11 @@
+number=int(input("Enter your number: "))
+
+table=[number*i for i in range(1,11)]
+print(table)
+
+'''Enter your number: 7
+[7, 14, 21, 28, 35, 42, 49, 56, 63, 70]
+
+prints 7 table in form of a list.
+example to show list comprehension makes the code easy in one line.
+'''
