ca91286c2ccfba0a0f77c83a7916ff817d0606a6
yukthi14 <109909210+yukthi14@users.noreply.github.com>
2022-10-18 08:15:34 +0530

    Pop up notification

---
+++ notification.py
@@ -0,0 +1,14 @@
+
+from plyer import notification
+
+if __name__ == "__main__":
+    
+    notification.notify(
+    title="***It's Time To Drink Water",
+    message="Wanna lose 1200 Calories a month? Drink a liter of ice water a day. You burn the energy just raising the water to body temp.",
+    app_icon="D:\PHYTON_PROGRAM\PYTHON\Iconsmind-Outline-Glass-Water.ico",
+    timeout=20
+    
+    
+)
+   
